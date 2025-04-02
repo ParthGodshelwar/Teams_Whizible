84770f16fd5a6e68acf4b16e8a1275c25a9b72c1
ParthGodshelwar <get15@whizible.com>
2025-04-02 11:57:21 +0530

    1157

--- src/app/auth/AuthGuard.jsx
+++ src/app/auth/AuthGuard.jsx
@@ -24,14 +24,16 @@ export default function AuthGuard({ children }) {
   // }, [isAuthenticated, isInitialized]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setInitialized(isInitialized);
-    }, 1000); // Adjust interval as needed (1000ms = 1 sec)
+    // const interval = setInterval(() => {
+    //   setInitialized(isInitialized);
+    // }, 1000); // Adjust interval as needed (1000ms = 1 sec)
 
-    return () => clearInterval(interval);
+    // return () => clearInterval(interval);
+
+    setInitialized(isInitialized);
   }, [isInitialized]);
 
-  if (!isInitialized) {
+  if (initialized) {
     // return <LoadingPage />; // Show loading spinner while initializing
     return <div>Loading AG11....</div>; // Show loading spinner while initializing
   }
