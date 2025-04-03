2af27517783aeab2da4dc32f878627b8991cf4b0
ParthGodshelwar <get15@whizible.com>
2025-04-03 11:48:27 +0530

    Refactor AuthGuard to show loading state and update JWTAuthContext reducer to maintain user state on INIT action

--- src/app/contexts/JWTAuthContext.js
+++ src/app/contexts/JWTAuthContext.js
@@ -25,7 +25,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "INIT": {
       const { isAuthenticated, user } = action.payload;
-      return { ...state, isAuthenticated, isInitialized: true, user };
+      return { ...state, isAuthenticated, user, isInitialized: true };
     }
     case "LOGIN":
       console.log("New state:", {
