769e138bcf72358656b6f496edaadb1616ac72ab
ParthGodshelwar <get15@whizible.com>
2025-04-03 16:16:21 +0530

    Comment out user authentication initialization in JWTAuthContext for future implementation

--- src/app/contexts/JWTAuthContext.js
+++ src/app/contexts/JWTAuthContext.js
@@ -69,20 +69,21 @@ export const AuthProvider = ({ children }) => {
   //   }
   // }, [navigate, isUnregistered]);
 
-  useEffect(() => {
-    const user = JSON.parse(sessionStorage.getItem("user"));
-    if (user) {
-      dispatch({ type: "INIT", payload: { user, isAuthenticated: true } });
-    } else {
-      dispatch({ type: "INIT", payload: { user: null, isAuthenticated: false } });
-    }
-  }, []);
+  // useEffect(() => {
+  //   const user = JSON.parse(sessionStorage.getItem("user"));
+  //   if (user) {
+  //     dispatch({ type: "INIT", payload: { user, isAuthenticated: true } });
+  //   } else {
+  //     dispatch({ type: "INIT", payload: { user: null, isAuthenticated: false } });
+  //   }
+  // }, []);
 
   const handleMicrosoftSignIn = async () => {
     try {
-      dispatch({ type: "LOGIN", payload: { isAuthenticated: true } });
-
+      // dispatch({ type: "LOGIN", payload: { isAuthenticated: true } });
+      console.log("Initializing Teams SDK...");
       await microsoftTeams.app.initialize();
+      dispatch({ type: "INIT", payload: { user: null, isAuthenticated: false } });
       console.log("Teams SDK initialized");
 
       microsoftTeams.authentication.getAuthToken({
