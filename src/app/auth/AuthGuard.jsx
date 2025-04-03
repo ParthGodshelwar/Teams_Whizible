a8fd4cc3dd52b77331cfa37bf34bae16d012d7a4
ParthGodshelwar <get15@whizible.com>
2025-04-03 11:54:52 +0530

    Add useEffect to AuthGuard for authentication state logging

--- src/app/auth/AuthGuard.jsx
+++ src/app/auth/AuthGuard.jsx
@@ -1,12 +1,19 @@
 import { Navigate, useLocation } from "react-router-dom";
 // HOOK
 import useAuth from "app/hooks/useAuth";
+import { useEffect } from "react";
 // CUSTOM COMPONENT
 
 export default function AuthGuard({ children }) {
   const { isAuthenticated, isInitialized } = useAuth();
   const { pathname } = useLocation();
   console.log("AuthGuard", isAuthenticated, isInitialized);
+
+  useEffect(() => {
+    // This effect runs when the component mounts and checks if the user is authenticated
+    // You can add any side effects or additional logic here if needed
+    console.log("AuthGuard effect triggered", isAuthenticated, isInitialized);
+  }, [isInitialized, isAuthenticated]);
 
   if (!isInitialized) {
     return <div>Loading...</div>; // Show a loading state until auth context is ready
